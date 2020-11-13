499d1b592c40b6b56ecfe6b0912e49a6fbc5bda5
Irina <ir1nak0z@yandex.ru>
2020-11-13 21:05:29 +0300

    Update app.c

--- RK3/some tasks/app.c
+++ RK3/some tasks/app.c
@@ -212,8 +212,150 @@ int filelen(FILE *f)
         len++;
     }
     
-    fseek(f , 0L, SEEK_SET); 
+    fseek(f , 0, SEEK_SET); 
     
     return len;
 }
 
+int file_size(FILE *f)
+{
+    int for_read, len = 0;
+
+    while (!feof(f))
+    {
+        if (fscanf(f, "%d", &for_read) == 1)
+        {
+            len += 1;
+        }
+        else if (!feof(f))
+        {
+            len = 0;
+            break;
+        }
+    }
+
+    return len;
+}
+
+int read_info_mat(FILE *f, int *n, int *m)
+{
+    int rc = OK;
+
+    rc = fscanf(f, "%d", n);
+    if (rc != 1 || *n <= 0)
+    {
+        rc = FILE_READ_ERR;
+        return rc;
+    }
+
+    rc = fscanf(f, "%d", m);
+    if (rc != 1 || *m <= 0)
+    {
+        rc = FILE_READ_ERR;
+        return rc;
+    }
+
+    return OK;
+}
+
+int read_matrix(FILE *f, double **matrix, int n, int m)
+{
+    int rc;
+    double mat_val;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            rc = fscanf(f, "%lf", &mat_val);
+            if (rc != 1)
+            {
+                rc = FILE_READ_ERR;
+                return rc;
+            }
+            matrix[i][j] = mat_val;
+        }
+    }
+
+    return OK;
+}
+
+void print_matrix(FILE *f, double **matrix, int n, int m)
+{
+    fprintf(f, "%d %d\n", n, m);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            fprintf(f, "%.6lf ", matrix[i][j]);
+        fprintf(f, "\n");
+    }
+}
+
+void parsing_to_file(int *pb, int *pe, FILE *f_out)
+{
+    while (pb < pe)
+    {
+        fprintf(f_out, "%d ", *pb);
+        pb++;
+    }
+}
+void parsing_array(FILE *f, int *pb)
+{
+    int for_read;
+    while (fscanf(f, "%d", &for_read) == 1)
+    {
+        *pb = for_read;
+        pb++;
+    }
+}
+
+int compare(const void *x1, const void *x2)
+{
+    const int *a = x1;
+    const int *b = x2;
+    return *a - *b;
+}
+void swap(void *first, void *second, size_t size)
+{
+    char t;
+    char *a = (char*)first;
+    char *b = (char*)second;
+
+    do
+    {
+        t = *a;
+        *a++ = *b;
+        *b++ = t;
+    }while (--size > 0);
+}
+void mysort(void *const first, size_t number, size_t size, int (*func_cmp)(const void *, const void *))
+{
+    char *pb = first;
+    char *pe = (char *)first + (number - 1) * size;
+
+    int flag = 1;
+
+    while ((pb < pe) && flag > 0)
+    {
+        flag = 0;
+        for (char *i = pb; i < pe; i += size)
+        {
+            if (func_cmp(i, i + size) > 0)
+            {
+                swap(i, i + size, size);
+                flag = 1;
+            }
+        }
+        pe -= size;
+
+        for (char *i = pe; i > pb; i -= size)
+        {
+            if (func_cmp(i - size, i) > 0)
+            {
+                swap(i - size, i, size);
+                flag = 1;
+            }
+        }
+        pb += size;
+    }
+}
