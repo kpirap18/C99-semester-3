dd86e2b546fff3c3ee62d02fe8277a6a9a173423
Irina <ir1nak0z@yandex.ru>
2021-01-14 18:24:15 +0300

    Add files via upload

---
+++ exam/string/my_getdelim.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int my_getdelim(char **lineptr, size_t *n, int delim, FILE *stream)
+{
+	*lineptr = NULL;
+	char *temp = NULL;
+    char c;
+    int rc = 0;
+    
+    size_t len = 0;
+    while (((c = fgetc(stream)) != delim) && (c != EOF) && (rc == 0))
+    {
+		len++;
+		temp = realloc((*lineptr), (len + 1) * sizeof(char));
+		if (temp != NULL)
+		{
+			(*lineptr) = temp;
+			(*lineptr)[len - 1] = c;
+		}
+		else
+		{
+			free(*lineptr);
+			rc = -1;
+		}			
+    }
+	if (rc == 0)
+	{
+		if (len == 0)
+		{
+			if (feof(stream))
+				rc = -1;
+			else
+				rc = -1;
+		}
+		else 
+			(*lineptr)[len] = '\0';
+	}
+    *n = len;
+    return rc;
+}
